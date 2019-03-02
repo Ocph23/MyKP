ed99e8853e7f7bbe97ba3b118b36932fe05b3672
ocph23 <ocph23@gmail.com>
2019-03-02 09:20:26 +0900

    #2 Maret 2019

---
+++ MainProject/DataAccesLayer/Models/role.cs
@@ -0,0 +1,31 @@
+﻿using Ocph.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Models
+{
+    [TableName("aspnetroles")]
+    public class role
+    {
+        [DbColumn("Id")]
+        public string Id { get; set; }
+        [DbColumn("Name")]
+        public string Name { get; set; }
+    }
+
+
+
+    [TableName("aspnetuserroles")]
+    public class userrole
+    {
+        [DbColumn("UserId")]
+        public string UserId { get; set; }
+        [DbColumn("RoleId")]
+        public string RoleId { get; set; }
+    }
+
+   
+}
---
+++ MainProject/TrireksaTracingApps/TrireksaTracingApps/Services/StatusService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TrireksaTracingApps.Models;
+
+namespace TrireksaTracingApps.Services
+{
+    public class StatusService : IDataStore<stt>
+    {
+        public async Task<bool> AddItemAsync(stt item)
+        {
+            try
+            {
+                using (var res = new RestServices())
+                {
+                    item.Status.RecieveDate = DateTime.Now;
+                    item.Status.STTId = item.Id;
+                    var result = await res.Post<status>("api/status", item.Status);
+                    if (result != null)
+                        return true;
+                    throw new SystemException("data tidak tersimpan");
+                }
+            }
+            catch (Exception ex)
+            {
+                Helper.ShowMessageError(ex.Message);
+                return false;
+            }
+        }
+
+        public Task<bool> DeleteItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<stt> GetItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<stt>> GetItemsAsync(bool forceRefresh = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateItemAsync(stt item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
